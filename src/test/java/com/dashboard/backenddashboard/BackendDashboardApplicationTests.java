3dad317495df6392b19b7adbdf16e58d9c3de8e0
guerreroeileen <eileenguerrerogomez@gmail.com>
2022-08-04 14:55:38 -0500

    initial commit

---
+++ src/test/java/com/dashboard/backenddashboard/BackendDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.backenddashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
